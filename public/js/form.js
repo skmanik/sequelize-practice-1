aef0029f4f8bd6c53b886c74e2fd9c5a4426f792
Sunny Manik <sundeepkmanik@gmail.com>
2019-05-15 23:06:29 -0700

    form submit

--- public/js/form.js
+++ public/js/form.js
@@ -1,3 +1,16 @@
 $(document).ready(() => {
 	console.log("Hello!");
+
+	$("#submit").click((event) => {
+		event.preventDefault();
+		let name = $("#name").val().trim();
+		let email = $("#email").val().trim();
+		let question = $("#question").val().trim();
+
+		console.log("Submit has been clicked!")
+
+		console.log("name", name);
+		console.log("email", email);
+		console.log("question", question);
+	});
 });
